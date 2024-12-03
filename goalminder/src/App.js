0632ac29415938159084c905618bd8fe32b0f3a2
echu-mohan <156306645+echu-mohan@users.noreply.github.com>
2024-12-03 15:59:54 +0100

    started new react project

---
+++ goalminder/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to the Task Planner</h1>
+        <p>It will save your day</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
